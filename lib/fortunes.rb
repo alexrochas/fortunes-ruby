567b5e67eb6c3530eee736fd1a9c1cf92095f1e6
Alex Rocha <alex.rochas@yahoo.com.br>
2016-07-16 20:47:08 -0300

    Refactor for a more functional approach

--- lib/fortunes.rb
+++ lib/fortunes.rb
@@ -14,16 +14,20 @@ class FortunesApp < Sinatra::Base
 
   get '/fortunes' do
     fortune_files.merge!(params){|key, oldval, newval| newval.to_i} unless params.empty?
-    read_file(Pickup.new(fortune_files).pick(1)) >> extract_sample >> generate_json
+    random_file = Pickup.new(fortune_files).pick(1)
+    random_file >> read_file >> extract_sample >> generate_json
   end
 
   get '/fortunes/:type' do
-    read_file(params['type']) >> extract_sample >> generate_json
+    file_name = params['type']
+    file_name >> read_file >> extract_sample >> generate_json
   end
 
-  def read_file(file_name)
-    fortune_path = File.join( File.dirname(__FILE__), "fortunes/#{file_name}" )
-    File.open(fortune_path).read.scan(/((?<=%).+?(?=%))/m)
+  def read_file()
+    -> (file_name) {
+      fortune_path = File.join( File.dirname(__FILE__), "fortunes/#{file_name}" )
+      File.open(fortune_path).read.scan(/((?<=%).+?(?=%))/m)
+    }
   end
 
   def extract_sample
